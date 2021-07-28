c300d5ad63fe487251de165b2f530bb56f392c48
Catur Satrio <52611164+Wong801@users.noreply.github.com>
2021-07-28 14:26:24 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://vite.nuxtjs.org/
-    'nuxt-vite',
+    // 'nuxt-vite',
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
